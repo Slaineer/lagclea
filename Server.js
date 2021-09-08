ff17240f6800969ea2b2604df3e9d62e92f3c1d9
Slaineer <89418489+Slaineer@users.noreply.github.com>
2021-09-09 02:03:12 +0300

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,66 @@
+const Server = [ 30 || 88 || 8080 || 5555 || 1337 || 2020 || 2000 ];
+
+ 
+const AloImports = {
+    AloServer: require('Serveric-General.js'),
+    AloUnit: require('Utils/Unit.js'),
+    Config: require(`servers/S${Server}.js`),
+    ConnectionMaster: require(`Engine/FluxEngine.js`),
+    myUnit: require('Utils/myUnit.js'),
+    EncodeByess: require('Utils/Encoder.js').bytes
+};
+
+let myServer = AloImports.AloServer( new Server( Server[AloServer] , i = "192.168.1.1" , l = 2 ));
+
+myServer.onopen = function (e) {
+    let o = new m$a(AloImports.AloUnit[12],AloImports.AloUnit[139],AloImports.AloUnit[52],AloImports.AloUnit[123],AloImports.AloUnit[5],AloImports.AloUnit[4],AloImports.AloUnit[1],AloImports.AloUnit[13],AloImports.AloUnit[3],AloImports.AloUnit[8],AloImports.AloUnit[42],AloImports.AloUnit[39],AloImports.AloUnit[9959],AloImports.AloUnit[0x0],AloImports.AloUnit[0x5],AloImports.AloUnit[0x13])
+    myServer.send(EncodeBytess(o))
+}
+let Alo_ = [ 1 , 2  , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16, 17 , 18 , 19 , 20 , true , false , ![] , !![] , 553 , 199] ,
+   c = Alo_[0],
+   v = Alo_[6];
+
+
+
+function m(c , v) {
+    if(c == [0]  - 5  * ( 3.12 * a>> 55)) {
+         for(let i = 0; i < Alo_[17]; i ++) {
+              let m$a = (v * c) + [i];
+              return m$a;
+         }
+    }
+}
+function m$a (a , b , c , d , e , f , g , h , i , j , k , l , m , n , o  , p , z) { 
+  
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = a ( b * 0.334 / m(c) * Alo_[5]) & d;
+    this.e = d ( typeof "string" ) && (a / b + e);
+    this.n = (g / 1.338 * 53 + l) & (h >> i ^ k * (((j * e >> 0 << 15.3449))))
+    this.z = m(g , n);
+    let m$K = function (k) { 
+        return ((k) ** d >> 55 ) & e;
+    }
+
+    this.update = function ($u_oU) {
+        switch ($u_oU) {
+            case e:
+                this.l = (f & m);
+                break;
+            case m$K:
+                console.log(b & a || 2 < p[5]);
+                break;
+            case o:
+                if(Alo_[20]) {
+                    console.log()
+                }
+                if(Alo_[21]) {
+                    console.log(`[Server] User Action ID:` + e + `\nInitiator:` + n)
+                }
+                break;
+        }
+    } 
+
+
+}
